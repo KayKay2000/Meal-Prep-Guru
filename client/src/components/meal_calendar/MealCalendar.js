330179d0031c5a87134af0193e3778a91afd2a32
Rick Rackley <93223405+safehaven1017@users.noreply.github.com>
2022-03-03 17:44:30 -0500

    added more css

--- client/src/components/meal_calendar/MealCalendar.js
+++ client/src/components/meal_calendar/MealCalendar.js
@@ -3,12 +3,14 @@ import styled from 'styled-components';
 import { useEffect, useState } from 'react';
 import MealSlot from './MealSlot';
 import axios from 'axios';
+import { Spinner } from '@chakra-ui/react'
 
 function MealCalendar() {
   const weekdayArray = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
   const [ plannerData, setPlannerData ] = useState({});
+  const apiKey = '68389b1f8db2442ab7f2595d12160698'
   useEffect(() => {
-    axios.get('https://api.spoonacular.com/mealplanner/safehaven1017/week/2022-02-28?hash=9b8c0e9c4a44720444ed3a25134e0e2d3358ff79&apiKey=9cf4d082fd9f4fdb90897ddfc4582935')
+    axios.get(`https://api.spoonacular.com/mealplanner/safehaven1017/week/2022-02-28?hash=9b8c0e9c4a44720444ed3a25134e0e2d3358ff79&apiKey=${apiKey}`)
     .then(res => {
       const mealPlan = res.data.days;
       const recipeIds = []; 
@@ -17,7 +19,7 @@ function MealCalendar() {
           recipeIds.push(item.value.id);
         })
       })
-      axios.get(`https://api.spoonacular.com/recipes/informationBulk?ids=${recipeIds.join(',')}&apiKey=9cf4d082fd9f4fdb90897ddfc4582935`).then(res => {
+      axios.get(`https://api.spoonacular.com/recipes/informationBulk?ids=${recipeIds.join(',')}&apiKey=${apiKey}`).then(res => {
         const imageObjectMap = {};
         res.data.forEach(recipe => {
           imageObjectMap[`${recipe.id}`] = recipe;
@@ -60,40 +62,77 @@ function MealCalendar() {
 
   return (
     <PageContainer>
-      {Object.keys(plannerData).length === 0 ?
-      <div>No Data</div> : (
-      <CalendarContainer>
-        {new Array(28).fill().map((_, index) => {
-            const weekday = weekdayArray[index % 7];
-            const mealPlanDay = getMealPlanDay(weekday, plannerData.mealPlan);
-            const slotData = mealPlanDay ? getSlotData(index, mealPlanDay) : '';
-            if (slotData.type === 'meals') {
-              slotData.items.forEach(item => {
-                item.imageLink = plannerData.imageObjectMap[`${item.value.id}`];
-              })
-            }
-            return <MealSlot slotData={slotData} key={index} />
-        })}
-      </CalendarContainer> )
-      }  
+      <DayAndGridContainer>
+        <DaysContainer>
+          {new Array(7).fill().map((_, index) => <Day key={index} >{weekdayArray[index]}</Day>)}
+        </DaysContainer>
+        {
+          Object.keys(plannerData).length === 0 ?
+          <Spinner size='xl' /> : (
+          <Grid>
+            {new Array(28).fill().map((_, index) => {
+                const weekday = weekdayArray[index % 7];
+                const mealPlanDay = getMealPlanDay(weekday, plannerData.mealPlan);
+                const slotData = mealPlanDay ? getSlotData(index, mealPlanDay) : '';
+                if (slotData.type === 'meals') {
+                  slotData.items.forEach(item => {
+                    item.imageLink = plannerData.imageObjectMap[`${item.value.id}`];
+                  })
+                }
+                return <MealSlot slotData={slotData} key={index} />
+            })}
+          </Grid> )
+        }  
+      </DayAndGridContainer>
     </PageContainer>
   )
 }
-
-const CalendarContainer = styled.div`
-  width: min-content;
-  height: min-content;
-  margin: 5vh;
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
-  box-shadow: 0px 0px .4vh .1vh #00000030;
-`
 
 const PageContainer = styled.div`
   width: 100vw;
   height: 100vh;
   display: flex;
   justify-content: center;
+  align-items: center;
+  margin-top: 6vw;
 `
 
-export default MealCalendar
+const DayAndGridContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: min-content;
+  height: min-content;
+`
+
+const Grid = styled.div`
+  width: min-content;
+  height: min-content;
+  margin: 5vh;
+  margin-top: 1vh;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+  box-shadow: 0px 0px .4vh .1vh #00000030;
+`
+
+const DaysContainer = styled.div`
+  width: 91vw;
+  height: 5vh;
+  display: flex;
+  justify-content: space-around;
+  margin: 0 5vh 0 5vh;
+`
+
+const Day = styled.div`
+  width: 9vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.5vw;
+  border-radius: 1vw;
+  text-align: center;
+  border-style: solid;
+  border-color: black;
+  font-weight: 200;
+`
+
+export default MealCalendar;
